b1da7ff6b399d11f091becf79feedc4cca6a031d
z-dev-34 <z.zekraoui.dev@gmail.com>
2023-04-02 18:12:28 +0200

    feat BO-3 create new todo and testing it

--- api/routes/todoController.test.js
+++ api/routes/todoController.test.js
@@ -17,6 +17,46 @@ const todos = [{
         isCompleted: false,
         createdAt: "2023-04-01",
         updatedAt: "2023-04-01"
+    },
+    {
+        id: 3,
+        title: "tache une",
+        description: "sefsdsdfsdf",
+        isCompleted: false,
+        createdAt: "2023-04-02",
+        updatedAt: "2023-04-02"
+    },
+    {
+        id: 4,
+        title: "tache une",
+        description: "sefsdsdfsdf",
+        isCompleted: false,
+        createdAt: "2023-04-02",
+        updatedAt: "2023-04-02"
+    },
+    {
+        id: 5,
+        title: "tache une",
+        description: "sefsdsdfsdf",
+        isCompleted: false,
+        createdAt: "2023-04-02",
+        updatedAt: "2023-04-02"
+    },
+    {
+        id: 9,
+        title: "tache new",
+        description: "test to test",
+        isCompleted: false,
+        createdAt: "2023-04-01",
+        updatedAt: "2023-04-02"
+    },
+    {
+        id: 10,
+        title: "tache new",
+        description: "test to test",
+        isCompleted: false,
+        createdAt: "2023-04-01",
+        updatedAt: "2023-04-02"
     }
 ]
 describe('GET /api/todos', () => {
@@ -26,7 +66,25 @@ describe('GET /api/todos', () => {
         expect(res.body).toEqual(todos);
     });
 });
-
+describe('POST /todos', () => {
+    test('creates a new todo item', async () => {
+        const todo = {
+            title: "tache new",
+            description: "test to test",
+            isCompleted: false,
+            createdAt: "2023-04-01",
+            updatedAt: "2023-04-01"
+        };
+        const response = await request(app)
+            .post('/api/todos')
+            .send(todo)
+            .set('Accept', 'application/json');
+        expect(response.status).toBe(201);
+        expect(response.body).toEqual({
+            message: "todo is created"
+        });
+    });
+});
 describe('PUT /todos/:id', () => {
     test('updates an existing todo item', async () => {
         const todo = {
@@ -42,6 +100,8 @@ describe('PUT /todos/:id', () => {
             .send(todo)
             .set('Accept', 'application/json');
         expect(response.status).toBe(200);
-        expect(response.body).toEqual(todo);
+        expect(response.body).toEqual({
+            message: "todo updated"
+        });
     });
 });
