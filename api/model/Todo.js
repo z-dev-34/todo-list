88cd451587a49560d5eb7817296af447e087e8a3
z-dev-34 <z.zekraoui.dev@gmail.com>
2023-04-11 23:26:21 +0000

    fix todo-BO-4: change response api

--- api/model/Todo.js
+++ api/model/Todo.js
@@ -3,7 +3,9 @@ module.exports = (sequelize, DataTypes) => {
     const Todo = sequelize.define('Todo', {
         id: {
             type: DataTypes.INTEGER,
-            primaryKey: true
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
         },
         title: {
             type: DataTypes.STRING,
--- api/routes/todoController.js
+++ api/routes/todoController.js
@@ -11,7 +11,6 @@ module.exports = {
             res.status(200).json(todos);
 
         } catch (error) {
-            console.error(error);
             res.status(500).json({
                 message: 'Server error'
             });
@@ -28,7 +27,6 @@ module.exports = {
                 res.status(200).json(todo);
             }
         } catch (error) {
-            console.error(error);
             res.status(500).json({
                 message: 'Server error'
             });
@@ -38,10 +36,10 @@ module.exports = {
         try {
             const todo = await models.Todo.create(req.body);
             res.status(201).json({
-                message: "todo is created"
+                message: "todo is created",
+                id: todo.id
             });
         } catch (error) {
-            console.error(error);
             res.status(500).json({
                 message: 'Server error'
             });
@@ -50,18 +48,19 @@ module.exports = {
     updateTodoById: async (req, res) => {
         try {
             const todo = await models.Todo.findByPk(req.params.id);
+            const newTodo = req.body
             if (!todo) {
                 res.status(404).json({
                     message: 'Todo not found'
                 });
             } else {
-                await todo.update(req.body);
+                newTodo.updatedAt = new Date()
+                await todo.update(newTodo);
                 res.status(200).json({
                     message: "todo updated"
                 });
             }
         } catch (error) {
-            console.error(error);
             res.status(500).json({
                 message: 'Server error'
             });
@@ -83,7 +82,6 @@ module.exports = {
                 });
             }
         } catch (error) {
-            console.error(error);
             res.status(500).json({
                 message: 'Server error'
             });
